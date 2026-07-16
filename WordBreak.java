40b13356078cdf155db240ecfb613a1b1704b099
Kavya <158807400+KavyaMunusamy@users.noreply.github.com>
2026-07-16 22:18:48 +0530

    classic Dynamic Programming (Word Break) problem
    Implement word break algorithm using dynamic programming.

---
+++ WordBreak.java
@@ -0,0 +1,17 @@
+class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> st = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n+1];
+        dp[0] = true;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && st.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
