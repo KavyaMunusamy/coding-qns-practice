8540bbf28425c37ad5dca6bc395e7ca089d18b76
Kavya <158807400+KavyaMunusamy@users.noreply.github.com>
2026-07-17 22:05:31 +0530

    Count ways with 3 moves
    Implemented a dynamic programming solution to count the number of ways to reach the nth step using 3 moves.
    A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time. Return the of count how many possible ways the child can run up the stairs.

---
+++ Countwayswith3moves.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    static int countWays(int n) {
+        // add your code here
+        int[] dp = new int[n+1];
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+}
